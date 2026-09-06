72b9c51c4d5f1a0c89799bb22e5eb4f0199ed6a8
Camila Battagliotti <battagliotticamila@gmail.com>
2026-09-06 16:11:13 +0200

    Configuracion inicial

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [],
+  providers: [],
   exports: [],
 })
 export class AppModule {}
